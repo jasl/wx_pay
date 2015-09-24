46d39014f109a44cc979d4d28c7776616066d102
Su Yijia <crystalremnet@gmail.com>
2015-09-24 18:05:30 +0800

    添加刷卡支付的接口。 WxPay::Service.invoke_micropay

--- lib/wx_pay/service.rb
+++ lib/wx_pay/service.rb
@@ -93,6 +93,22 @@ module WxPay
       r
     end
 
+    INVOKE_MICROPAY_REQUIRED_FIELDS = %i(body out_trade_no total_fee spbill_create_ip auth_code)
+    def self.invoke_micropay(params)
+      params = {
+        appid: WxPay.appid,
+        mch_id: WxPay.mch_id,
+        nonce_str: SecureRandom.uuid.tr('-', '')
+      }.merge(params)
+      puts params
+      check_required_options(params, INVOKE_MICROPAY_REQUIRED_FIELDS)
+
+      r = invoke_remote "#{GATEWAY_URL}/pay/micropay", make_payload(params)
+
+      yield(r) if block_given?
+
+      r
+    end
 
     private
 
