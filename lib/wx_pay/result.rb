b7b68e84d300ab9e36cded8bafed3e9ab1086429
houdelin <houdelin@bayekeji.com>
2017-01-20 14:58:54 +0800

    add raw xml in result

--- lib/wx_pay/result.rb
+++ lib/wx_pay/result.rb
@@ -5,8 +5,9 @@ module WxPay
     def initialize(result)
       super nil # Or it will call `super result`
 
-      self[:raw] = result
       result_hash = Hash.from_xml(result)
+      self[:raw] = result_hash
+      self[:raw_xml] = result
       if result_hash['xml'].class == Hash
         result_hash['xml'].each_pair do |k, v|
           self[k] = v
