a4304ba8e6fa6bfc611a40e589d4b6f70f3b3f65
fxg <fengxinguo@outlook.com>
2017-09-01 22:21:40 +0800

    let 'WxPay::Sign.verify?' support options, just like all of other functions

--- lib/wx_pay/sign.rb
+++ lib/wx_pay/sign.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module WxPay
   module Sign
     def self.generate(params)
-      key = params.delete(:key)
+      key = params.delete('key') || params.delete(:key)
 
       query = params.sort.map do |k, v|
         "#{k}=#{v}" if v.to_s != ''
@@ -12,8 +12,10 @@ module WxPay
       Digest::MD5.hexdigest("#{query}&key=#{key || WxPay.key}").upcase
     end
 
-    def self.verify?(params)
+    def self.verify?(params, options = {})
       params = params.dup
+      params = params.merge(options)
+      
       sign = params.delete('sign') || params.delete(:sign)
 
       generate(params) == sign
