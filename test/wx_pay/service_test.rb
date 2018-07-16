0a426320b7637af2f245e02771cad3f6129f483e
moohao <32318384+moohao@users.noreply.github.com>
2018-07-16 10:32:04 +0800

    fix service_test.rb

--- test/wx_pay/service_test.rb
+++ test/wx_pay/service_test.rb
@@ -48,7 +48,7 @@ class ServiceTest < MiniTest::Test
       mch_id: 'mch_id',
       key: 'key'
     }
-    xml_str = '<xml><body>测试商品</body><out_trade_no>test003</out_trade_no><total_fee>1</total_fee><spbill_create_ip>127.0.0.1</spbill_create_ip><notify_url>http://making.dev/notify</notify_url><trade_type>JSAPI</trade_type><openid>OPENID</openid><app_id>app_id</app_id><mch_id>mch_id</mch_id><sign>172A2D487A37D13FDE32B874BA823DD6</sign></xml>'
+    xml_str = '<xml><app_id>app_id</app_id><body>测试商品</body><mch_id>mch_id</mch_id><notify_url>http://making.dev/notify</notify_url><openid>OPENID</openid><out_trade_no>test003</out_trade_no><spbill_create_ip>127.0.0.1</spbill_create_ip><total_fee>1</total_fee><trade_type>JSAPI</trade_type><sign>172A2D487A37D13FDE32B874BA823DD6</sign></xml>'
     assert_equal xml_str, WxPay::Service.send(:make_payload, params)
   end
 end
